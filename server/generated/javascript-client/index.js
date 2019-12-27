236bbe7ada9b70092b66568305eef6af873e4f04
Iván Rodríguez <ivanrodricalleja@gmail.com>
2019-12-27 18:18:47 +0100

    Post 2 setup prisma

--- server/generated/javascript-client/index.js
+++ server/generated/javascript-client/index.js
@@ -9,25 +9,26 @@ var models = [
     embedded: false
   },
   {
-    name: "OrderStatus",
-    embedded: false
-  },
-  {
     name: "Order",
     embedded: false
   },
   {
-    name: "Product",
+    name: "OrderLine",
     embedded: false
   },
   {
-    name: "OrderLine",
+    name: "OrderStatus",
+    embedded: false
+  },
+  {
+    name: "Product",
     embedded: false
   }
 ];
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `http://localhost:4466`
+  endpoint: `http://localhost:4466`,
+  secret: `mySuperSecret`
 });
 exports.prisma = new exports.Prisma();
